e939e77ae0f1e8172e00084460706160766ae26f
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-22 16:11:33 -0400

    chore: use txHistory objects instead

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2176,6 +2176,7 @@ export default class MetamaskController extends EventEmitter {
       this.controllerMessenger.getRestricted({
         name: BRIDGE_STATUS_CONTROLLER_NAME,
         allowedActions: [
+          'AccountsController:getSelectedAccount',
           'NetworkController:getSelectedNetworkClient',
           'NetworkController:findNetworkClientIdByChainId',
         ],
