297b685493cb4f2acf9cff34b4b8fcbf9415bbaa
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-23 11:46:52 -0400

    fix: not being able to fetch pending bridge status, should not cache status fetch

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -2180,10 +2180,9 @@ export default class MetamaskController extends EventEmitter {
           'NetworkController:getSelectedNetworkClient',
           'NetworkController:findNetworkClientIdByChainId',
         ],
-        allowedEvents: ['TransactionController:transactionConfirmed'],
+        allowedEvents: [],
       });
     this.bridgeStatusController = new BridgeStatusController({
-      provider: this.provider,
       messenger: bridgeStatusControllerMessenger,
     });
 
